a5107c4f2247cf930e02ddaeb58a9d9ada2439bc
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-15 23:59:56 +0100

    country types

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type Country = {
+    Country: string;
+    CountryCode: string;
+    Date: string;
+    ID: string;
+    NewConfirmed: number;
+    NewDeaths: number;
+    NewRecovered: number;
+    Premium: unknown;
+    slug: string;
+    TotalConfirmed: number;
+    ToTalDeaths: number;
+    TotalRecovered: number;
+}
