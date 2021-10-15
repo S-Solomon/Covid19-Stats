0b2d5637c4851b88dd7cbc000a0ba15c34b2a0c5
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-16 00:01:06 +0100

    typing the global and response data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react'
+
 
 
 const App: React.FunctionComponent = () => {
+  const [data, setData] = useState(undefined)
+  
+  const fetchData = async () => {
+    const result  = await fetch('https://api.covid19api.com/');
+    const data = await result.json;
+
+    setData(data);
+  };
+
+  useEffect(() => {
+    console.log(data)
+  })
+
     return(
       <div className="app">
         <h1>hello</h1>
--- src/types/types.ts
+++ src/types/types.ts
@@ -12,3 +12,21 @@ export type Country = {
     ToTalDeaths: number;
     TotalRecovered: number;
 }
+
+export type GlobalData = {
+    Date: string;
+    NewConfirmed: number;
+    NewDeaths: number;
+    NewRecovered: number;
+    TotalConfirmed: number;
+    TotalDeaths: number;
+    TotalRecovered: number;
+};
+
+export type ResponseData = {
+    Countries: Country[];
+    Date: string;
+    Global: GlobalData;
+    ID: string;
+    Message: string;
+};
