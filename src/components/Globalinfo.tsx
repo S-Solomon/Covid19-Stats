69f58ffac1550d3bb72173c6e2772324d17e7e5d
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-16 10:19:42 +0100

    global info component + the Intl object

---
+++ src/components/Globalinfo.tsx
@@ -0,0 +1,26 @@
+interface Props {
+    newConfirmed: number;
+    newDeaths: number;
+    newRecovered: number;
+}
+
+const Globalinfo: React.FunctionComponent<Props> = ({
+    newConfirmed,
+    newDeaths,
+    newRecovered,
+}) => {
+    return (
+        <div>
+            <h1>Global Covid-19 data</h1>
+            <h3>
+                New Confirmed: {new Intl.NumberFormat().format(newConfirmed)}
+            </h3>
+            <h3>New Deaths: {new Intl.NumberFormat().format(newDeaths)}</h3>
+            <h3>
+                New Recovered: {new Intl.NumberFormat().format(newRecovered)}
+            </h3>
+        </div>
+    );
+};
+
+export default Globalinfo;
